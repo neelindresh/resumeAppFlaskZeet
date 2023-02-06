44c9be5771c0cb1ff6fdaa48d283e5b5532af5fb
neelindresh <indresh2neel@gmail.com>
2023-02-06 22:38:29 +0530

    till skills

---
+++ data/details.py
@@ -0,0 +1,88 @@
+ABOUT_ME={
+    "position":"Data Scientist",
+    "about": "A self-motivated, tech-savvy and result oriented professional who always looks for opportunity to exercise creative and innovative ideas to make contribution to the development of the organization. Experienced in creating end-to- end Analytics solutions.",
+    "intro": "A self-motivated, tech-savvy and result oriented professional who always looks for opportunity to exercise creative and innovative ideas to make contribution to the development of the organization. Experienced in creating end-to- end Analytics solutions.",
+    
+}
+
+ABOUT_DETAILS={
+    "years_of_exp": "05+",
+    "projects": "15+",
+    "clients": "05+"
+}
+
+SKILLS=[
+    {
+        "header":"Machine Learning",
+        "exp":"05+",
+        "tools":[
+            {
+                    "name":"Scikit-learn",
+                    "perct":90,
+            },
+            {
+                    "name":"Pandas",
+                    "perct":70,
+            },
+            {
+                    "name":"Numpy",
+                    "perct":70,
+            },
+            {
+                    "name":"Predictive Modeling",
+                    "perct":80,
+            },
+            {
+                    "name":"Statistical Modeling",
+                    "perct":80,
+            },
+            {
+                    "name":"Forcasting",
+                    "perct":60,
+            },
+            {
+                    "name":"Supervised Learning",
+                    "perct":90,
+            },
+            {
+                    "name":"Unsupervised Learning",
+                    "perct":80,
+            },
+        ]
+    },
+    {
+        "header":"Deep Learning",
+        "exp":"05+",
+        "tools":[
+            {
+                    "name":"Pytorch",
+                    "perct":90,
+            },
+            {
+                    "name":"Tensorflow",
+                    "perct":50,
+            },
+            {
+                    "name":"Computer Vision (CV)",
+                    "perct":70,
+            },
+            {
+                    "name":"Natural Language Processing(NLP)",
+                    "perct":80,
+            },
+            {
+                    "name":"Natural Language Understanding(NLU)",
+                    "perct":80,
+            },
+            {
+                    "name":"Generative Modeling (GANS, Transformers)",
+                    "perct":60,
+            },
+            
+        ]
+    }
+    
+]
+    
+    
+    
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import flask
-
+from data import details
 app=flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
-
+    return flask.render_template("index.html",
+                                 about_me=details.ABOUT_ME,
+                                 )
+@app.route("/jinja/")
+def index_jinja():
+    return flask.render_template("index-jinja.html",
+                                 about_me=details.ABOUT_ME,
+                                 about_details=details.ABOUT_DETAILS,
+                                 skills=details.SKILLS
+                                 )
 
 if __name__=="__main__":
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0",port=5005)
